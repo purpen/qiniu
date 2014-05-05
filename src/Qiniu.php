49e13d5f4db26062314119f5b65da727444953ab
purpen <purpen.w@gmail.com>
2014-05-05 18:26:04 +0800

    add new class.

---
+++ src/Qiniu.php
@@ -0,0 +1,14 @@
+<?php
+
+class Qiniu
+{
+    /**
+     * Factory a client
+     *
+     * @param array $options
+     * @return Client
+     */
+    public static function create(array $options){
+        return new Client($options);
+    }
+}
